632090ebc9eee96d1b51094b2b59bcdc4e9056b7
Kensuke Yanai <y2aiskni@gmail.com>
2024-12-03 00:12:31 +0900

    feat: asdfをインストールするスクリプト追加

---
+++ scripts/README.md
@@ -0,0 +1,7 @@
+# scripts
+
+## asdfをインストール
+
+```shel
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/y2aiskni/lima-config/refs/heads/main/scripts/install-asdf.sh)"
+```
---
+++ scripts/install-asdf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eu -o pipefail
+
+if [ $(uname -s) != "Linux" ]; then
+  echo "error: Your platform is not supported."
+  exit 1
+fi
+
+if command -v asdf > /dev/null 2>&1; then
+  echo "exit: Already installed asdf"
+  exit 0
+fi
+
+if !(command -v jq > /dev/null 2>&1); then
+  echo "error: Please install the jq command"
+  exit 1
+fi
+
+echo -e "+++ get version +++"
+ASDF_LATEST_VERSION=$(curl -sSL https://api.github.com/repos/asdf-vm/asdf/releases/latest | jq -r .tag_name)
+
+echo -e "\n+++ clone repository... +++"
+git clone --branch $ASDF_LATEST_VERSION --depth 1 https://github.com/asdf-vm/asdf.git ~/.asdf
+
+echo -e "\n+++ set up shell +++"
+cat << 'EOF' >> ~/.bashrc
+
+# BEGIN asdf
+. "$HOME/.asdf/asdf.sh"
+. "$HOME/.asdf/completions/asdf.bash"
+# END asdf
+EOF
+
+echo -e "\ncomplete!"
