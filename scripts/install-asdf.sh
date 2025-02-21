70c456e874e96515256a078448aa25134eb30111
Kensuke Yanai <y2aiskni@gmail.com>
2025-02-22 02:53:30 +0900

    fix: linux arm64

--- scripts/install-asdf.sh
+++ scripts/install-asdf.sh
@@ -6,6 +6,16 @@ if [ $(uname -s) != "Linux" ]; then
   exit 1
 fi
 
+OS_ARCH=$(uname -m)
+if [ "$OS_ARCH" == "aarch64"]; then
+  OS_ARCH="arm64"
+fi
+
+if [ "$OS_ARCH" != "amd64" ] && [ "$OS_ARCH" != "arm64" ]; then
+  echo "error: Your architecture is not supported."
+  exit 1
+fi
+
 if command -v asdf > /dev/null 2>&1; then
   echo "exit: Already installed asdf"
   exit 0
@@ -18,7 +28,6 @@ fi
 
 echo "# Install asdf CLI"
 ASDF_LATEST_VERSION=$(curl -sSL https://api.github.com/repos/asdf-vm/asdf/releases/latest | jq -r .tag_name)
-OS_ARCH=$(uname -m)
 curl -sL "https://github.com/asdf-vm/asdf/releases/download/${ASDF_LATEST_VERSION}/asdf-${ASDF_LATEST_VERSION}-linux-${OS_ARCH}.tar.gz" | sudo tar -zxv -C /usr/local/bin
 echo ""
 
